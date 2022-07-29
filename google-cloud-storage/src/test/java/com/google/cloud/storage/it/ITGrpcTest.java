3f122cc7e7d049b60183ce470ab9cb009d758411
Sydney Munro <sydmunro@google.com>
2022-07-29 13:15:10 -0700

    Trying out Parameterized tests

--- google-cloud-storage/src/test/java/com/google/cloud/storage/it/ITGrpcTest.java
+++ google-cloud-storage/src/test/java/com/google/cloud/storage/it/ITGrpcTest.java
@@ -27,38 +27,58 @@ import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.StorageFixture;
 import com.google.cloud.storage.StorageOptions;
 import com.google.cloud.storage.conformance.retry.CleanupStrategy;
+import com.google.cloud.storage.conformance.retry.ParallelParameterized;
 import com.google.cloud.storage.conformance.retry.TestBench;
 import com.google.cloud.storage.testing.RemoteStorageHelper;
 import com.google.common.collect.ImmutableList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.logging.Logger;
 import java.util.stream.StreamSupport;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(ParallelParameterized.class)
 public final class ITGrpcTest {
+  private static final Logger LOGGER = Logger.getLogger(ITGrpcTest.class.getName());
   @ClassRule(order = 1)
   public static final TestBench TEST_BENCH =
       TestBench.newBuilder().setContainerName("it-grpc").setDockerImageTag("v0.26.0").build();
 
-  @ClassRule(order = 2)
-  public static final StorageFixture storageFixture =
-      StorageFixture.from(
-          () ->
-              StorageOptions.grpc()
-                  .setHost(TEST_BENCH.getGRPCBaseUri())
-                  .setCredentials(NoCredentials.getInstance())
-                  .setProjectId("test-project-id")
-                  .build());
+  @Rule
+  public final StorageFixture storageFixture;
 
-  @ClassRule(order = 3)
-  public static final BucketFixture bucketFixture =
-      BucketFixture.newBuilder()
-          .setBucketNameFmtString("java-storage-gcs-grpc-team-%s")
-          .setCleanupStrategy(CleanupStrategy.ALWAYS)
-          .setHandle(storageFixture::getInstance)
-          .build();
+  @Rule
+  public final BucketFixture bucketFixture;
+
+  public ITGrpcTest(StorageFixture storageFixture) {
+    this.storageFixture = storageFixture;
+    this.bucketFixture = BucketFixture.newBuilder()
+        .setBucketNameFmtString("java-storage-gcs-grpc-team-%s")
+        .setCleanupStrategy(CleanupStrategy.ALWAYS)
+        .setHandle(storageFixture::getInstance)
+        .build();
+  }
+
+  @Parameters( name = "{0}")
+  public static Collection<StorageFixture> data() {
+    StorageFixture grpcStorageFixture = StorageFixture.from(
+        () ->
+            StorageOptions.grpc()
+                .setHost(TEST_BENCH.getGRPCBaseUri())
+                .setCredentials(NoCredentials.getInstance())
+                .setProjectId("test-project-id")
+                .build());
+    StorageFixture jsonStorageFixture = StorageFixture.defaultHttp();
+    return Arrays.asList(grpcStorageFixture, jsonStorageFixture);
+  }
 
   @Test
   public void testCreateBucket() {
+    LOGGER.info("Running testCreateBucket with " + storageFixture.getInstance().getOptions().getHost());
     final String bucketName = RemoteStorageHelper.generateBucketName();
     Bucket bucket = storageFixture.getInstance().create(BucketInfo.of(bucketName));
     assertThat(bucket.getName()).isEqualTo(bucketName);
@@ -66,6 +86,7 @@ public final class ITGrpcTest {
 
   @Test
   public void listBlobs() {
+    LOGGER.info("Running listBlobs with " + storageFixture.getInstance().getOptions().getHost());
     BucketInfo bucketInfo = bucketFixture.getBucketInfo();
     Page<Blob> list = storageFixture.getInstance().list(bucketInfo.getName());
     ImmutableList<String> bucketNames =
@@ -78,6 +99,7 @@ public final class ITGrpcTest {
 
   @Test
   public void listBuckets() {
+    LOGGER.info("Running listBuckets with " + storageFixture.getInstance().getOptions().getHost());
     Page<Bucket> list = storageFixture.getInstance().list();
     ImmutableList<String> bucketNames =
         StreamSupport.stream(list.iterateAll().spliterator(), false)
